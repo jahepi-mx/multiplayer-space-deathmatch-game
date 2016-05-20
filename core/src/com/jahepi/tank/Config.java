7b30b86188cb9a281300eca5f8ee567e93288bc8
jvhernandez <jvhernandez@pennsylvania.com.mx>
2016-05-20 13:16:06 -0500

    Fixed missile position on client ships, change the shooting logic.

--- core/src/com/jahepi/tank/Config.java
+++ core/src/com/jahepi/tank/Config.java
@@ -4,17 +4,17 @@ public class Config {
 
 	public static final String VERSION = "v1.2";
 	public static final String ADMOB_KEY = "ca-app-pub-3227769552322799/6244007069";
-	
+
 	public static final float WIDTH = 21.33f * 3.0f;
 	public static final float HEIGHT = 16 * 3;
 	public static final float CAMERA_WIDTH = WIDTH / 3;
 	public static final float CAMERA_HEIGHT = HEIGHT / 3;
 	public static final float MAP_SCALE_FACTOR = 0.2f;
 	public static final float CAMERA_CENTER_DISTANCE = 18.0f;
-	
+
 	public static final int UI_WIDTH = 640;
 	public static final int UI_HEIGHT = 480;
-	
+
 	// Camera constants for nicknames display
 	public static final float UI_CAMERA_WIDTH = 1440;
 	public static final float UI_CAMERA_HEIGHT = 1080;
@@ -22,9 +22,9 @@ public class Config {
 	public static final float UI_CAMERA_HEIGHT_RATIO = CAMERA_HEIGHT / HEIGHT;
 	public static final float UI_WIDTH_RATIO = UI_CAMERA_WIDTH / WIDTH;
 	public static final float UI_HEIGHT_RATIO = UI_CAMERA_HEIGHT / HEIGHT;
-	
+
 	public static final boolean DEBUG = false;
-	
+
 	private Config() {
 		// TODO Auto-generated constructor stub
 	}
