2fd63fa12503b6d542755a923eb1e778f0ec7df4
jahepi <jahepi@gmail.com>
2016-07-14 02:19:54 -0500

    Config change.

--- core/src/com/jahepi/tank/Config.java
+++ core/src/com/jahepi/tank/Config.java
@@ -2,7 +2,7 @@ package com.jahepi.tank;
 
 public class Config {
 
-	public static final String VERSION = "v1.2";
+	public static final String VERSION = "v1.3";
 	public static final String ADMOB_KEY = "ca-app-pub-3227769552322799/6244007069";
 	public static final String ADMOB_INTERSTITIAL_KEY = "ca-app-pub-3227769552322799/1942964667";
 	public static final String ADMOB_TEST_ADS_KEY = "91225038BBD19AC2FC79B5F07EB41AF8";
