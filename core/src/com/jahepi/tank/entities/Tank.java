2d632ed621355f18b617376c04394f04c720a890
jahepi <jahepi@gmail.com>
2016-07-24 16:19:53 -0500

    Minor chages.

--- core/src/com/jahepi/tank/entities/Tank.java
+++ core/src/com/jahepi/tank/entities/Tank.java
@@ -56,7 +56,7 @@ public class Tank extends GameEntity {
 	protected int missileDamage = 1;
 	protected TEXTURE_MISSILE_TYPE missileTextureType;
 	protected float missileEffectScale = Config.MIN_EXPLOSION_SIZE;
-	protected float missileSpeed = 16.0f;
+	protected float missileSpeed = 14.0f;
 	protected TEXTURE_TYPE textureType;
 	protected BitmapFont font;
 	protected float speedUpTime;
